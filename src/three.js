4a9b7ef55e63bab2e89d779fe368d698f5955ab3
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-13 20:25:14 +0700

    [FEATURE] Finished Number three

--- src/three.js
+++ src/three.js
@@ -1,10 +1,14 @@
 let cards = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
 function three(n,times,cards){
-    for(let i = 0; i < times; i++){
-        const bottomCards = cards.splice(-n);
-        cards = [...bottomCards, ...cards];
+    try{
+        for(let i = 0; i < times; i++){
+            const bottomCards = cards.splice(-n);
+            cards = [...bottomCards, ...cards];
+        }
+        console.log(`kartu tumpukan taratas dari ${n} kartu, ${times} kali : ${cards[0]}`);
+    } catch(err){
+        console.log(err);
     }
-    console.log(`kartu tumpukan taratas : ${cards[0]}`);
 }
 three(5,45,cards); //5 pengambilan kartu , 45 kali 
 three(7,50,cards); //7 pengambilan kartu, 50 kali 
--- src/two.js
+++ src/two.js
@@ -1,18 +1,20 @@
 
-function two(){
-    let cara = 0;
-    let totalPermen = 12;
-
-    for(let i = 7; i <= totalPermen; i++){
-        let sisa = totalPermen - i;
-        for(let j = 0; j <= sisa; j++){
-            const k = sisa - j;
-            console.log(`Banyak Permen Abi : ${j} | Banyak Permen Bibi : ${k} | Banyak Permen Cibi : ${i}`);
-            cara++;
+function two(totalPermen){
+    let cara = 0
+    try{
+        for(let i = 7; i <= totalPermen; i++){
+            let sisa = totalPermen - i;
+            for(let j = 0; j <= sisa; j++){
+                const k = sisa - j;
+                console.log(`Permen Abi : ${j} | Permen Bibi : ${k} | Permen Cibi : ${i}`);
+                cara++;
+            }
         }
+        const responseCara = `banyak cara ${cara}`;
+        return responseCara;
+    } catch(err){
+        console.log(err);
     }
-    const responseCara = `banyak cara ${cara}`;
-    return responseCara;
 }
 
-console.log(two());
+console.log(two(12));
